7f33e77913ecd50db0d52c8a79cc32a24dd8e135
Ayush Rana <ayushrana182@gmail.com>
2021-04-19 12:24:49 +0545

    Basic Types

---
+++ basic-types.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+exports.dumpStringOrNumber = exports.dumpObject = exports.introducePerson = exports.introduction = exports.addNumbers = exports.isACat = void 0;
+var isACat = function (animal) { return animal === "cat"; };
+exports.isACat = isACat;
+function addNumbers(a, b) {
+    return a + b;
+}
+exports.addNumbers = addNumbers;
+var introduction = function (name) { return "Hello " + name; };
+exports.introduction = introduction;
+var introducePerson = function (name) { return console.log(exports.introduction(name)); };
+exports.introducePerson = introducePerson;
+var dumpObject = function (type, obj) { return type + ":" + JSON.stringify(obj); };
+exports.dumpObject = dumpObject;
+var dumpStringOrNumber = function (info) { return "value " + info; };
+exports.dumpStringOrNumber = dumpStringOrNumber;
---
+++ test.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var basic_types_1 = require("./basic-types");
+console.log(basic_types_1.isACat("cat"));
+console.log(basic_types_1.isACat("dog"));
+console.log("");
+console.log(basic_types_1.addNumbers(1, 2));
+console.log(basic_types_1.addNumbers(1, "Jack"));
+console.log("");
+console.log(basic_types_1.introduction("Jack"));
+console.log("");
+basic_types_1.introducePerson("Molly");
+console.log("");
+console.log(basic_types_1.dumpObject("object", {}));
+console.log("");
+console.log(basic_types_1.dumpStringOrNumber("foo"));
+console.log(basic_types_1.dumpStringOrNumber(10));
